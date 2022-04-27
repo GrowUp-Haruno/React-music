915418f6c0be06c1a96174b5fb5ea11e96c1570b
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-04-27 16:21:14 +0900

    - 音量平均の算出方法見直し - 音量の標準偏差を追加

--- src/learning/Mic.tsx
+++ src/learning/Mic.tsx
@@ -1,6 +1,8 @@
-import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import { Button, Divider, Stack, Text } from '@chakra-ui/react';
 import { MouseEventHandler, useCallback, useEffect, useRef, useState } from 'react';
 
+const initialMeasureResult = { time: 0, aveVolume: 0, sdVolume: 0 };
+const initialSumMaxdb: number[] = [];
 const initilaFFTsize = 2048;
 const samplingRate = 100; // [ms]
 
@@ -16,13 +18,14 @@ export const Mic = () => {
   const tackTime = useRef<Date>();
   const endTime = useRef<Date>();
   const reqRef = useRef<number>();
-  const sumMaxdb = useRef<{ index: number; sumData: number }>({ index: 0, sumData: 0 });
+  const sumMaxdb = useRef<number[]>(initialSumMaxdb);
 
   const [playing, setPlaying] = useState(false);
   const [maxFreq, setmaxFreq] = useState({ index: 0, data: 0 });
-  const [measureResult, setMeasureResult] = useState({ time: 0, aveVolume: 0 });
+  const [measureResult, setMeasureResult] = useState(initialMeasureResult);
 
-  const handleOnClick: MouseEventHandler<HTMLButtonElement> = async (event) => {
+  // 収音開始
+  const handlePlay: MouseEventHandler<HTMLButtonElement> = async (event) => {
     try {
       if (playing) return;
       audioCtx.current = new AudioContext();
@@ -41,6 +44,12 @@ export const Mic = () => {
     }
   };
 
+  // 最大データをリセット
+  const handleReset = () => {
+    setMeasureResult(initialMeasureResult);
+  };
+
+  // 解析及び最高記録の計算処理
   const loop = useCallback(() => {
     if (analyser.current && dbData.current) {
       analyser.current.getFloatFrequencyData(dbData.current);
@@ -71,10 +80,9 @@ export const Mic = () => {
         tackTime.current = new Date();
         if (
           tackTime.current.getTime() - startTime.current.getTime() >
-          sumMaxdb.current.index * samplingRate
+          sumMaxdb.current.length * samplingRate
         ) {
-          sumMaxdb.current.index++;
-          sumMaxdb.current.sumData += Math.abs(dbMax.data);
+          sumMaxdb.current.push(Math.abs(dbMax.data));
         }
       }
 
@@ -85,16 +93,30 @@ export const Mic = () => {
 
           // 最大時間を更新したらmeasureResultを更新する
           if (measureResult.time < endTime.current.getTime() - startTime.current.getTime()) {
+            const resultTime = endTime.current.getTime() - startTime.current.getTime();
+
+            const resultAveVolume =
+              sumMaxdb.current.reduce((prev, current) => prev + 10 ** (current / 20), 0) /
+              sumMaxdb.current.length;
+
+            const resultSdVolume = Math.sqrt(
+              sumMaxdb.current.reduce(
+                (prev, current) => prev + (10 ** (current / 20) - resultAveVolume) ** 2,
+                0
+              ) / sumMaxdb.current.length
+            );
+
             setMeasureResult({
-              time: endTime.current.getTime() - startTime.current.getTime(),
-              aveVolume: sumMaxdb.current.sumData / sumMaxdb.current.index,
+              time: resultTime,
+              aveVolume: 20 * Math.log10(resultAveVolume),
+              sdVolume: resultSdVolume,
             });
           }
 
           startTime.current = undefined;
           tackTime.current = undefined;
           endTime.current = undefined;
-          sumMaxdb.current = { index: 0, sumData: 0 };
+          sumMaxdb.current = initialSumMaxdb;
         }
       }
     }
@@ -112,15 +134,25 @@ export const Mic = () => {
 
   return (
     <Stack spacing={4}>
-      <Button onClick={handleOnClick} w={20} bg={'blue.100'}>
-        Play
-      </Button>
-      <Box>
-        <Text>{`周波数　　：${(maxFreq.index * (44100 / initilaFFTsize)).toFixed(2)}[Hz]`}</Text>
-        <Text>{`音量　　　：${maxFreq.data.toFixed(2)}[db]`}</Text>
-        <Text>{`最高記録　：${(measureResult.time / 1000).toFixed(2)}[s]`}</Text>
+      <Stack spacing={2}>
+        <Text>{`周波数：${(maxFreq.index * (44100 / initilaFFTsize)).toFixed(2)}[Hz]`}</Text>
+        <Text>{`音量：${maxFreq.data.toFixed(2)}[db]`}</Text>
+        <Divider />
+        <Text>{`最高記録：${(measureResult.time / 1000).toFixed(2)}[s]`}</Text>
         <Text>{`最高記録時の平均音量：-${measureResult.aveVolume.toFixed(2)}[dB]`}</Text>
-      </Box>
+        <Text>{`音量の標準偏差：${measureResult.sdVolume.toFixed(2)}`}</Text>
+        <Text color='gray'>{`音量の標準偏差は0に近いほどバラツキが少ない`}</Text>
+      </Stack>
+
+      {!playing ? (
+        <Button onClick={handlePlay} w={20} colorScheme="blue">
+          計測開始
+        </Button>
+      ) : (
+        <Button onClick={handleReset} w={20} colorScheme="green">
+          リセット
+        </Button>
+      )}
     </Stack>
   );
 };
