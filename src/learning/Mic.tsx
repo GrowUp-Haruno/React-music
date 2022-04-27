334dbac4c2691d6072d9a05f0dbf73d169103e09
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-04-27 14:05:20 +0900

    最大持続時間の音量平均を求めるロジックを追加

--- src/learning/Mic.tsx
+++ src/learning/Mic.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Stack, Text } from '@chakra-ui/react';
 import { MouseEventHandler, useCallback, useEffect, useRef, useState } from 'react';
 
 const initilaFFTsize = 2048;
+const samplingRate = 100; // [ms]
 
 export const Mic = () => {
   const stream = useRef<MediaStream>();
@@ -9,15 +10,17 @@ export const Mic = () => {
   const audioCtx = useRef<AudioContext>();
   const analyser = useRef<AnalyserNode>();
   const bufferLength = useRef<number>();
-  const data = useRef<Uint8Array>();
+  // const data = useRef<Uint8Array>();
   const dbData = useRef<Float32Array>();
   const startTime = useRef<Date>();
+  const tackTime = useRef<Date>();
   const endTime = useRef<Date>();
   const reqRef = useRef<number>();
+  const sumMaxdb = useRef<{ index: number; sumData: number }>({ index: 0, sumData: 0 });
 
   const [playing, setPlaying] = useState(false);
   const [maxFreq, setmaxFreq] = useState({ index: 0, data: 0 });
-  const [maxTime, setMaxTime] = useState(0);
+  const [measureResult, setMeasureResult] = useState({ time: 0, aveVolume: 0 });
 
   const handleOnClick: MouseEventHandler<HTMLButtonElement> = async (event) => {
     try {
@@ -28,7 +31,7 @@ export const Mic = () => {
       analyser.current = audioCtx.current.createAnalyser();
       analyser.current.fftSize = initilaFFTsize;
       bufferLength.current = analyser.current.frequencyBinCount;
-      data.current = new Uint8Array(bufferLength.current);
+      // data.current = new Uint8Array(bufferLength.current);
       dbData.current = new Float32Array(bufferLength.current);
       mediaStreamSource.current.connect(analyser.current);
 
@@ -42,33 +45,61 @@ export const Mic = () => {
     if (analyser.current && dbData.current) {
       analyser.current.getFloatFrequencyData(dbData.current);
       const dbMax = {
+        // 周波数(この値に(44100 / initilaFFTsize)を掛けると算出可能)
         index: 0,
+        // 音量(0~1の不動小数点をdbに変換した値が入る)
         data: -Infinity,
       };
+
+      // FFTデータの内、音量が最大となる周波数(dbData.index)と音量(dbMax.date)を取得する
       for (let i = 0; i < dbData.current.length; i++) {
         if (dbData.current[i] > dbMax.data) {
           dbMax.index = i;
           dbMax.data = dbData.current[i];
         }
       }
+
+      // データ更新
       setmaxFreq(dbMax);
+
+      // 測定開始処理
       if (dbMax.data > -50) {
+        // 測定開始時刻を取得
         if (!startTime.current) startTime.current = new Date();
+
+        // 測定中の最大音量の平均値を取得
+        tackTime.current = new Date();
+        if (
+          tackTime.current.getTime() - startTime.current.getTime() >
+          sumMaxdb.current.index * samplingRate
+        ) {
+          sumMaxdb.current.index++;
+          sumMaxdb.current.sumData += Math.abs(dbMax.data);
+        }
       }
+
+      // 測定終了処理
       if (dbMax.data < -50) {
         if (startTime.current && !endTime.current) {
           endTime.current = new Date();
 
-          if (maxTime < endTime.current.getTime() - startTime.current.getTime())
-            setMaxTime(endTime.current.getTime() - startTime.current.getTime());
+          // 最大時間を更新したらmeasureResultを更新する
+          if (measureResult.time < endTime.current.getTime() - startTime.current.getTime()) {
+            setMeasureResult({
+              time: endTime.current.getTime() - startTime.current.getTime(),
+              aveVolume: sumMaxdb.current.sumData / sumMaxdb.current.index,
+            });
+          }
 
           startTime.current = undefined;
+          tackTime.current = undefined;
           endTime.current = undefined;
+          sumMaxdb.current = { index: 0, sumData: 0 };
         }
       }
     }
     reqRef.current = requestAnimationFrame(loop);
-  }, [maxTime]);
+  }, [measureResult]);
 
   useEffect(() => {
     loop();
@@ -87,7 +118,8 @@ export const Mic = () => {
       <Box>
         <Text>{`周波数　　：${(maxFreq.index * (44100 / initilaFFTsize)).toFixed(2)}[Hz]`}</Text>
         <Text>{`音量　　　：${maxFreq.data.toFixed(2)}[db]`}</Text>
-        <Text>{`最高記録　：${(maxTime / 1000).toFixed(2)}[s]`}</Text>
+        <Text>{`最高記録　：${(measureResult.time / 1000).toFixed(2)}[s]`}</Text>
+        <Text>{`最高記録時の平均音量：-${measureResult.aveVolume.toFixed(2)}[dB]`}</Text>
       </Box>
     </Stack>
   );
